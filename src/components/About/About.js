13c8a72ef141f22d6720e8723cd1fa86110b7f70
HaAymar <he201905@students.ephec.be>
2023-11-30 14:59:51 +0100

    Update the last version of portfolio

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,19 +2,20 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Techstack from "./Techstack";
 import Toolstack from "./Toolstack";
-
+import { useTranslation } from "react-i18next";
 function Skills() {
+	const { t } = useTranslation("skills");
 	return (
 		<Container style={{ padding: "80px", color: "white" }}>
 			<Container>
 				<h1 className="project-heading">
-					<strong className="purple">My Skills </strong>
+					<strong className="purple">{t("skills")}</strong>
 				</h1>
 
 				<Techstack />
 
 				<h1 className="project-heading">
-					<strong className="purple">Tools</strong>
+					<strong className="purple">{t("tools")}</strong>
 				</h1>
 				<Toolstack />
 			</Container>
